4d4ca8d6f8d6f42fb9d5b5089d30c6a465c38ea1
Bastian de Byl <bastian@debyl.io>
2025-02-17 11:22:19 -0500

    noticket - update VAT rate for finland

--- common.go
+++ common.go
@@ -33,33 +33,33 @@ var (
 
 	BuildVersion string             = "development"
 	EUCountryVAT map[string]float64 = map[string]float64{
-		"at": 0.20, // Austria
-		"be": 0.21, // Belgium
-		"bg": 0.20, // Bulgaria
-		"hr": 0.25, // Croatia
-		"cy": 0.19, // Cyprus
-		"cz": 0.21, // Czech Republic
-		"dk": 0.25, // Denmark
-		"ee": 0.20, // Estonia
-		"fi": 0.24, // Finland
-		"fr": 0.20, // France
-		"de": 0.19, // Germany
-		"gr": 0.24, // Greece
-		"hu": 0.27, // Hungary
-		"ie": 0.23, // Ireland, Republic of (EIRE)
-		"it": 0.22, // Italy
-		"lv": 0.21, // Latvia
-		"lt": 0.21, // Lithuania
-		"lu": 0.16, // Luxembourg
-		"mt": 0.18, // Malta
-		"nl": 0.21, // Netherlands
-		"pl": 0.23, // Poland
-		"pt": 0.23, // Portugal
-		"ro": 0.19, // Romania
-		"sk": 0.20, // Slovakia
-		"si": 0.22, // Slovenia
-		"es": 0.21, // Spain
-		"se": 0.25, // Sweden
+		"at": 0.20,  // Austria
+		"be": 0.21,  // Belgium
+		"bg": 0.20,  // Bulgaria
+		"hr": 0.25,  // Croatia
+		"cy": 0.19,  // Cyprus
+		"cz": 0.21,  // Czech Republic
+		"dk": 0.25,  // Denmark
+		"ee": 0.20,  // Estonia
+		"fi": 0.255, // Finland
+		"fr": 0.20,  // France
+		"de": 0.19,  // Germany
+		"gr": 0.24,  // Greece
+		"hu": 0.27,  // Hungary
+		"ie": 0.23,  // Ireland, Republic of (EIRE)
+		"it": 0.22,  // Italy
+		"lv": 0.21,  // Latvia
+		"lt": 0.21,  // Lithuania
+		"lu": 0.16,  // Luxembourg
+		"mt": 0.18,  // Malta
+		"nl": 0.21,  // Netherlands
+		"pl": 0.23,  // Poland
+		"pt": 0.23,  // Portugal
+		"ro": 0.19,  // Romania
+		"sk": 0.20,  // Slovakia
+		"si": 0.22,  // Slovenia
+		"es": 0.21,  // Spain
+		"se": 0.25,  // Sweden
 	}
 
 	// Non-delivery Options
